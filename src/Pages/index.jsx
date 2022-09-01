bea4ec81c35103a50312bc198884c353bae41ae9
PoornaSankalana <sanka2016pps@gmail.com>
2022-09-01 12:18:30 +0530

    Exporting pages

---
+++ src/Pages/index.jsx
@@ -0,0 +1,5 @@
+import Home from "./Home/Home";
+import Register from "./Patient/Register/Register";
+import PatientUpdate from "./Patient/Update/Update";
+
+export { Home, Register, PatientUpdate };
