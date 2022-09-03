6f888dd594b7ae40988ecf5fcff781fd59130a74
Poorna Sankalana <83366788+PoornaSankalana@users.noreply.github.com>
2022-09-03 21:05:30 +0530

    exporting report table and login pages

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -1,5 +1,7 @@
 import Home from "./Home/Home";
 import Register from "./Patient/Register/Register";
 import PatientUpdate from "./Patient/Update/Update";
+import LoginPage from "./LoginPage/LoginPage";
+import ReportTable from "./Report/TableView/ReportTable";
 
-export { Home, Register, PatientUpdate };
+export { Home, Register, PatientUpdate, LoginPage, ReportTable };
