33bfd831220f39a5a59afca224bc7a39d496233f
Poorna Sankalana <83366788+PoornaSankalana@users.noreply.github.com>
2022-09-03 21:03:41 +0530

    add backend base urls

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.headers.common["authentication"] =
+	localStorage.getItem("authentication");
 
 ReactDOM.render(
 	<React.StrictMode>
