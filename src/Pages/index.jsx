ca4a2e4e58548486136db8b9ce913a8b1cce643b
Poorna Sankalana <83366788+PoornaSankalana@users.noreply.github.com>
2022-09-29 22:54:38 +0530

    exporting pages

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -3,5 +3,15 @@ import Register from "./Patient/Register/Register";
 import PatientUpdate from "./Patient/Update/Update";
 import LoginPage from "./LoginPage/LoginPage";
 import PatientTableList from "./Patient/View/Table/patientTableList";
+import AddAppointmentPage from "./Appointment/AppointmentAddPage/AddAppointment";
+import AboutUsPage from "./AboutUsPage/AboutUsPage";
 
-export { Home, Register, PatientUpdate, LoginPage, PatientTableList };
+export {
+	Home,
+	Register,
+	PatientUpdate,
+	LoginPage,
+	PatientTableList,
+	AddAppointmentPage,
+	AboutUsPage,
+};
