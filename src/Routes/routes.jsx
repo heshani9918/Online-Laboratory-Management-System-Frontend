573ef022aa6361123f33e7cc30f6cb44269fd3a8
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2022-10-01 11:47:46 +0530

    implementing routes

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -7,6 +7,10 @@ import {
 	LoginPage,
 	ReportTable,
 	AddReportPage,
+	AddAppointmentPage,
+	AppointmentTable,
+    AppointmentCheckPage,
+	AppointmentCancel,
 } from "../Pages";
 
 const PageRoutes = () => {
@@ -32,6 +36,22 @@ const PageRoutes = () => {
 						<Route
 							path="/patient/report/add"
 							element={<AddReportPage />}
+						/>
+						<Route
+							path="/patient/appointment/add"
+							element={<AddAppointmentPage />}
+						/>
+						<Route
+							path="/patient/appointment/all"
+							element={<AppointmentTable />}
+						/>
+						<Route
+							path="/patient/appointment"
+							element={<AppointmentCheckPage />}
+						/>
+						<Route
+							path="/patient/appointment/cancel"
+							element={<AppointmentCancel />}
 						/>
 					</Routes>
 				</div>
